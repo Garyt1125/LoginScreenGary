56d9988eb0dbc18e7079a1e449f37788e20b33a4
Gary <gary.tapia@brooklynsteamcenter.org>
2019-06-13 14:16:06 -0400

    Initial Commit

---
+++ LoginScreenGary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoginScreenGary
+//
+//  Created by Genesis on 6/13/19.
+//  Copyright © 2019 Genesis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
